4754f03c04c35cee0df1ef678c76193f7920745c
Karl Beksa <karlbeksa@gmail.com>
2019-07-16 11:06:29 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TeamMember from './components/TeamMember';
-import Form from './components/Form';
-
-
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TeamMember from "./components/TeamMember";
+import Form from "./components/Form";
 
 function App() {
-
   const [member, setMember] = useState({
-    name: '',
-    email: '',
-    role: '',
+    name: "",
+    email: "",
+    role: "",
     update: false
   });
 
-  const [list, setList] = useState ([]);
+  const [list, setList] = useState([]);
 
-  const setUpdateMember = (name) => {
-    let updated = list.filter(item => item.name === name)
-    setMember({...updated, update: true})
-  }
+  const setUpdateMember = name => {
+    let updated = list.filter(item => item.name === name);
+    setMember({ ...updated, update: true });
+  };
   const updateMember = (name, update) => {
-    let updated = list.filter(item => item.name !== name)
+    let updated = list.filter(item => item.name !== name);
     setList([...updated, update]);
-    setMember({...update, update: false})
+    setMember({ ...update, update: false });
+  };
 
-
-  }
-
-  const addMember = (add) => {
+  const addMember = add => {
     setList([...list, add]);
-  }
+  };
   return (
     <div className="App">
-      {list.map(member => <TeamMember member={member} setUpdateMember={setUpdateMember} key={member.name} />)}
-      <Form member={member} updateMember={updateMember} addMember={addMember} key={member.name} />
+      {list.map(member => (
+        <TeamMember
+          member={member}
+          setUpdateMember={setUpdateMember}
+          key={member.name}
+        />
+      ))}
+      <Form
+        member={member}
+        updateMember={updateMember}
+        addMember={addMember}
+        key={member.name}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import './component.css';
+import React, { useState, useEffect } from "react";
+import "./component.css";
 
 export default function Form(props) {
+  const [formMember, setFormMember] = useState({});
+  let name;
+  if (props.member[0]) {
+    name = props.member[0].name;
+  }
 
-    const [formMember, setFormMember] = useState({});
-    let name;
-    if(props.member[0]) {name=props.member[0].name}
+  useEffect(() => {
+    setFormMember({ ...props.member[0] });
+  }, []);
 
-        useEffect(() => {
+  // }
 
-            setFormMember({...props.member[0]})
+  const handleChange = e => {
+    setFormMember({ ...formMember, [e.target.name]: e.target.value });
+  };
 
-        },[])
-    
-    // }
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.member.update
+      ? props.updateMember(name, formMember)
+      : props.addMember(formMember);
+    setFormMember({ name: "", email: "", role: "", update: false });
+  };
 
-    const handleChange = e => {
-        setFormMember({...formMember, [e.target.name]: e.target.value});
-    }
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.member.update ? props.updateMember(name, formMember) : 
-        props.addMember(formMember);
-        setFormMember({name: '', email: '', role: '', update: false});
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name: &nbsp;
-                    <input type='text' onChange={handleChange} name='name' placeholder='name' value={formMember.name} />
-                </label>
-                <label>
-                    Email: &nbsp;
-                    <input type='email' onChange={handleChange} name='email' placeholder='email' value={formMember.email} />
-                </label>
-                <label>
-                    Role: &nbsp;
-                    <input type='text' onChange={handleChange} name='role' placeholder='role' value={formMember.role} />
-                </label>
-                <button> submit </button>
-            
-            
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name: &nbsp;
+          <input
+            type="text"
+            onChange={handleChange}
+            name="name"
+            placeholder="name"
+            value={formMember.name}
+          />
+        </label>
+        <label>
+          Email: &nbsp;
+          <input
+            type="email"
+            onChange={handleChange}
+            name="email"
+            placeholder="email"
+            value={formMember.email}
+          />
+        </label>
+        <label>
+          Role: &nbsp;
+          <input
+            type="text"
+            onChange={handleChange}
+            name="role"
+            placeholder="role"
+            value={formMember.role}
+          />
+        </label>
+        <button> submit </button>
+      </form>
+    </div>
+  );
 }
